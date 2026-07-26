8b064302c2c35726c6d35f4aa5e21aa8c3155b77
chuka <chuka@chuknzeka.co.uk>
2026-07-26 14:26:39 +0100

    Day 5 - remote state with S3 and DynamoDB

--- main.tf
+++ main.tf
@@ -5,7 +5,14 @@ terraform {
             version = "~>5.0"
         }
     }
+backend "s3" {
+    bucket = "chuka-terraform-state-577638397757"
+    key            = "day5/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "chuka-terraform-state-lock"
+    encrypt        = true
 }
+}  
 
 provider "aws" {
     region = var.region
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
     type = string
-    default = "us-east-1"
+    default     = "us-east-1"
     description = "The AWS region to deploy resources in"
 }
 
